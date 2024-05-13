db591ccd054348b9155c96cacfbec1aea7377169
Jan Niklas Drescher <jan.drescher@tu-braunschweig.de>
2024-05-13 14:10:55 +0200

    use tmpfs to store temporal fuzzer data

--- docker/generate_compose.py
+++ docker/generate_compose.py
@@ -7,7 +7,9 @@ PATH_MAP = {
     "chrome": "./chrome-ipc-fuzzing/src/out/Default/chrome",
     }
 
-def generate_docker_compose(browser, num_services):
+def generate_docker_compose(browser, num_services, tmpfs):
+    tmp_data_root = "/dev/shm/ipcfuzzing" if tmpfs is True else "."
+
     content = "services:"
 
     for service_id in range(num_services):
@@ -33,9 +35,9 @@ def generate_docker_compose(browser, num_services):
       core: -1
     volumes:
       - ..:/app
-      - ./volumes/{service_id}/origin-1:/app/server/origin-1
-      - ./volumes/{service_id}/origin-2:/app/server/origin-2
-      - ./volumes/{service_id}/logs:/app/logs
+      - {tmp_data_root}/volumes/{service_id}/origin-1:/app/server/origin-1
+      - {tmp_data_root}/volumes/{service_id}/origin-2:/app/server/origin-2
+      - {tmp_data_root}/volumes/{service_id}/logs:/app/logs
       - ./volumes/{service_id}/crash:/app/crash
       - ./volumes/{service_id}/coredumps:/coredumps
     working_dir: /app
@@ -43,13 +45,20 @@ def generate_docker_compose(browser, num_services):
 """
     return content
 
-def make_dirs(num_services):
+def make_dirs(num_services, tmpfs):
     for service_id in range(num_services):
-        os.makedirs(f"./volumes/{service_id}/origin-1", exist_ok=True)
-        os.makedirs(f"./volumes/{service_id}/origin-2", exist_ok=True)
-        os.makedirs(f"./volumes/{service_id}/logs", exist_ok=True)
         os.makedirs(f"./volumes/{service_id}/crash", exist_ok=True)
         os.makedirs(f"./volumes/{service_id}/coredumps", exist_ok=True)
+
+        if tmpfs is True:
+          os.makedirs(f"/dev/shm/ipcfuzzing/volumes/{service_id}/origin-1", exist_ok=True)
+          os.makedirs(f"/dev/shm/ipcfuzzing/volumes/{service_id}/origin-2", exist_ok=True)
+          os.makedirs(f"/dev/shm/ipcfuzzing/volumes/{service_id}/logs", exist_ok=True)
+        else:
+          os.makedirs(f"./volumes/{service_id}/origin-1", exist_ok=True)
+          os.makedirs(f"./volumes/{service_id}/origin-2", exist_ok=True)
+          os.makedirs(f"./volumes/{service_id}/logs", exist_ok=True)
+            
 
 def main():
     parser = argparse.ArgumentParser(description='Generate a docker-compose.yml file for multiple services.')
@@ -60,14 +69,20 @@ def main():
         type=str,
         choices=["chrome", "firefox"],
     )
-    parser.add_argument('num_services', type=int, help='Number of services to generate in the docker-compose file')
+    parser.add_argument(
+        "-t",
+        "--tmpfs",
+        help="store inputs and logs on tmpfs",
+        action="store_true"
+    )
+    parser.add_argument("-n", "--num_services", type=int, help='Number of services to generate in the docker-compose file')
     args = parser.parse_args()
     if args.num_services < 1:
         print("Number of services must be at least 1.")
         return
 
-    make_dirs(args.num_services)
-    docker_compose_content = generate_docker_compose(args.browser, args.num_services)
+    make_dirs(args.num_services, args.tmpfs)
+    docker_compose_content = generate_docker_compose(args.browser, args.num_services, args.tmpfs)
     with open("docker-compose.yml", "w") as file:
         file.write(docker_compose_content)
 
